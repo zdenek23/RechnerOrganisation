1ad421f12e1f2715c212f28021e80c6a683433be
Zdenek Zeman <zzeman@student.tugraz.at>
2018-06-01 13:59:57 +0200

    c-program

---
+++ programme/C/a0.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+
+int i;
+int X = 1;
+int digit = 0;
+int buffer[16] = {0};
+int* buffer_ptr = buffer;
+int halt = 0;
+int plus = 0x802B;
+int multiplyer = 0x802A;
+int newline = 0x800A;
+int result_ctrl = 0;
+int number_ctrl = 0;
+
+void my_getchar()
+{
+    i = getchar();
+}
+
+void my_printf(int* value, int nl)
+{
+    printf("%x", *value);
+    if (nl) printf("%x", newline);
+}
+
+void convert()
+{
+    if(digit) X *= 10;
+    else X = 0;
+    X = X + (i - 0x30);
+    digit = 1;
+}
+
+void save()
+{
+    *buffer_ptr++ = X;
+    digit = 0;
+}
+
+void add()
+{
+    X += *--buffer_ptr;
+}
+
+void multiply()
+{
+    X *= *--buffer_ptr;
+}
+
+void check_char()
+{
+    if (i==EOF)
+    {
+        halt = 1;
+    }
+    else if (i > 0x2f & i < 0x3a)
+    {
+        convert();
+        number_ctrl = 1;
+    }
+    else if (i==0x0a)
+    {
+        save();
+        if (result_ctrl == 0) my_printf(&X, 1);
+        result_ctrl = 0;
+        number_ctrl = 0;
+    }
+    else if (i==0x2b)
+    {
+        if (number_ctrl>0) my_printf(&X, 0);
+        else if (result_ctrl>0) my_printf(&X, 0);
+        my_printf(&plus, 1);
+        add();
+        my_printf(&X, 1);
+        result_ctrl = 1;
+    }
+    else if (i==0x2a)
+    {
+        if (number_ctrl>0) my_printf(&X, 0);
+        else if (result_ctrl>0) my_printf(&X, 0);
+        my_printf(&multiplyer, 1);
+        multiply();
+        my_printf(&X, 1);
+        result_ctrl = 1;
+    }
+}
+
+int main()
+{
+    while(1)
+    {
+        my_getchar();
+        check_char();
+        if (halt) break;
+    }
+    return 0;
+}
---
+++ programme/C/shell_script.sh
@@ -0,0 +1,112 @@
+#! /bin/bash
+
+echo "a1.c"
+gcc -o a1 a1.c
+
+echo "a1.c with test_input_rpn0.txt" 
+./a1 < testbed/test_inputs/input_rpn0.txt > testbed/test_out.txt
+    
+diff -q testbed/test_out.txt testbed/expected_outputs/expected_output_rpn0.txt > tmp_files/result.txt
+    
+if [[ -s tmp_files/result.txt ]];
+    then
+      echo "ERROR"
+    else
+      echo "CORRECT"
+    fi
+
+echo "a1.c with test_input_rpn0m.txt" 
+./a1 < testbed/test_inputs/input_rpn0m.txt > testbed/test_out.txt
+    
+diff -q testbed/test_out.txt testbed/expected_outputs/expected_output_rpn0m.txt > tmp_files/result.txt
+    
+if [[ -s tmp_files/result.txt ]];
+    then
+      echo "ERROR"
+    else
+      echo "CORRECT"
+    fi
+
+echo "a1.c with test_input_rpn1.txt" 
+./a1 < testbed/test_inputs/input_rpn1.txt > testbed/test_out.txt
+    
+diff -q testbed/test_out.txt testbed/expected_outputs/expected_output_rpn1.txt > tmp_files/result.txt
+    
+if [[ -s tmp_files/result.txt ]];
+    then
+      echo "ERROR"
+    else
+      echo "CORRECT"
+    fi
+
+echo "a1.c with test_input_rpn1m.txt" 
+./a1 < testbed/test_inputs/input_rpn1m.txt > testbed/test_out.txt
+    
+diff -q testbed/test_out.txt testbed/expected_outputs/expected_output_rpn1m.txt > tmp_files/result.txt
+    
+if [[ -s tmp_files/result.txt ]];
+    then
+      echo "ERROR"
+    else
+      echo "CORRECT"
+    fi
+
+echo "a1.c with test_input_rpn2.txt" 
+./a1 < testbed/test_inputs/input_rpn2.txt > testbed/test_out.txt
+    
+diff -q testbed/test_out.txt testbed/expected_outputs/expected_output_rpn2.txt > tmp_files/result.txt
+    
+if [[ -s tmp_files/result.txt ]];
+    then
+      echo "ERROR"
+    else
+      echo "CORRECT"
+    fi
+
+echo "a1.c with test_input_rpn2m.txt" 
+./a1 < testbed/test_inputs/input_rpn2m.txt > testbed/test_out.txt
+    
+diff -q testbed/test_out.txt testbed/expected_outputs/expected_output_rpn2m.txt > tmp_files/result.txt
+    
+if [[ -s tmp_files/result.txt ]];
+    then
+      echo "ERROR"
+    else
+      echo "CORRECT"
+    fi
+
+echo "a1.c with test_input_rpn3.txt" 
+./a1 < testbed/test_inputs/input_rpn3.txt > testbed/test_out.txt
+    
+diff -q testbed/test_out.txt testbed/expected_outputs/expected_output_rpn3.txt > tmp_files/result.txt
+    
+if [[ -s tmp_files/result.txt ]];
+    then
+      echo "ERROR"
+    else
+      echo "CORRECT"
+    fi
+
+echo "a1.c with test_input_rpn3m.txt" 
+./a1 < testbed/test_inputs/input_rpn3m.txt > testbed/test_out.txt
+    
+diff -q testbed/test_out.txt testbed/expected_outputs/expected_output_rpn3m.txt > tmp_files/result.txt
+    
+if [[ -s tmp_files/result.txt ]];
+    then
+      echo "ERROR"
+    else
+      echo "CORRECT"
+    fi
+
+echo "a1.c with test_input_rpn4.txt" 
+./a1 < testbed/test_inputs/input_rpn4.txt > testbed/test_out.txt
+    
+diff -q testbed/test_out.txt testbed/expected_outputs/expected_output_rpn4.txt > tmp_files/result.txt
+    
+if [[ -s tmp_files/result.txt ]];
+    then
+      echo "ERROR"
+    else
+      echo "CORRECT"
+    fi
